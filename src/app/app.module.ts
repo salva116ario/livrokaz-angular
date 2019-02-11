ecb09645aa680899fa1d23fd05f83ff4cde42b73
GORCE Pierre <looupsing@gmail.com>
2019-02-11 17:09:32 +0100

    add material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './view/footer/footer.component';
 import { HeaderComponent } from './view/header/header.component';
 import { BookDetailComponent } from './view/book-detail/book-detail.component';
+import { MatIconModule } from '@angular/material/icon';
+import { IconSvgComponent } from './view/icon-svg/icon-svg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    IconSvgComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
