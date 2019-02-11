3bfd1c3322456308599f25ff4f6b814ed7de535c
Kaspatou <thomas.longueville.lp2@gmail.com>
2019-02-11 15:06:30 +0100

    getter setter & constructor angular

--- src/app/model/role.model.ts
+++ src/app/model/role.model.ts
@@ -1,6 +1,49 @@
+import {Customer} from './customer.model';
+import {Employee} from './employee.model';
+
 export class Role {
   private roId: number;
   private roLabel: string;
   private customers: Customer[];
   private employees: Employee[];
+
+
+  constructor(roId: number, roLabel: string, customers: Customer[], employees: Employee[]) {
+    this.roId = roId;
+    this.roLabel = roLabel;
+    this.customers = customers;
+    this.employees = employees;
+  }
+
+  get _roId(): number {
+    return this.roId;
+  }
+
+  set _roId(value: number) {
+    this.roId = value;
+  }
+
+  get _roLabel(): string {
+    return this.roLabel;
+  }
+
+  set _roLabel(value: string) {
+    this.roLabel = value;
+  }
+
+  get _customers(): Customer[] {
+    return this.customers;
+  }
+
+  set _customers(value: Customer[]) {
+    this.customers = value;
+  }
+
+  get _employees(): Employee[] {
+    return this.employees;
+  }
+
+  set _employees(value: Employee[]) {
+    this.employees = value;
+  }
 }
--- src/app/model/style.model.ts
+++ src/app/model/style.model.ts
@@ -1,8 +1,49 @@
+import {Book} from './book.model';
+
 export class Style {
   private stId: number;
   private stLabelEn: string;
   private stLabelFr: string;
   private books: Book[];
-}
 
+
+  constructor(stId: number, stLabelEn: string, stLabelFr: string, books: Book[]) {
+    this.stId = stId;
+    this.stLabelEn = stLabelEn;
+    this.stLabelFr = stLabelFr;
+    this.books = books;
+  }
+
+  get _stId(): number {
+    return this.stId;
+  }
+
+  set _stId(value: number) {
+    this.stId = value;
+  }
+
+  get _stLabelEn(): string {
+    return this.stLabelEn;
+  }
+
+  set _stLabelEn(value: string) {
+    this.stLabelEn = value;
+  }
+
+  get _stLabelFr(): string {
+    return this.stLabelFr;
+  }
+
+  set _stLabelFr(value: string) {
+    this.stLabelFr = value;
+  }
+
+  get _books(): Book[] {
+    return this.books;
+  }
+
+  set _books(value: Book[]) {
+    this.books = value;
+  }
+}
 
