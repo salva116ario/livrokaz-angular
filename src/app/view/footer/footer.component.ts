882c4c0926dd2050722718a326cfa8a0b538f227
GORCE Pierre <looupsing@gmail.com>
2019-02-12 10:00:48 +0100

    fix merge

--- src/app/view/footer/footer.component.ts
+++ src/app/view/footer/footer.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
+  isAdmin: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.isAdmin = false;
   }
 
+  ngOnInit() {}
+
+  adminDisConnect(){
+    console.log('Connection');
+  }
 }
